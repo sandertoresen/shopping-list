c2a291c821563b2741433541806fc84effcb5770
sandertoresen <sandertoresen@icloud.com>
2024-03-05 16:26:12 +0100

    added blueprint .NET project

---
+++ APIHandler/ShoppingListAPI/ShoppingListAPI/Controllers/ShoppingListController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingListAPI.models;
+
+namespace ShoppingListAPI.Controllers;
+
+public class ShoppingListController : ControllerBase
+{
+    [HttpGet("/shopping-list")]
+    public IActionResult GetShoppingList()
+    {
+        var mockData = new List<Item>
+        {
+            new Item() { Id = 0, Name = "Milk", Count = 2 },
+            new Item() { Id = 1, Name = "Egg", Count = 3 },
+            new Item() { Id = 2, Name = "Flour", Count = 12 },
+        };
+
+        return Ok(mockData);
+    }
+    
+    [HttpPost("/shopping-list")]
+    public IActionResult PostShoppingList([FromBody] Item item)
+    {
+        //TODO: post
+
+        return Ok();
+    }
+    
+    [HttpDelete("/shopping-list/{id}")]
+    public IActionResult DeleteShoppingList(int id)
+    {
+        //TODO: delete
+        return Ok();
+    }
+}
---
+++ APIHandler/ShoppingListAPI/ShoppingListAPI/models/item.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingListAPI.models;
+
+public struct Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
